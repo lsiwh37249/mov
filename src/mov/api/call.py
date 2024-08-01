dd519c7abc7de799e373eeb817393448343ab167
lsiwh37249 <lsiwh37249@gmail.com>
2024-08-01 18:35:52 +0900

    update call.py

--- src/mov/api/call.py
+++ src/mov/api/call.py
@@ -9,15 +9,15 @@ def echo(yaho):
 
 def apply_type2df(load_dt="20120101", path="~/tmp/test_parquet"):
     df = pd.read_parquet(f'{path}/load_dt={load_dt}')
-    df['rnum'] = pd.to_numeric(df['rnum'])
-    df['rank'] = pd.to_numeric(df['rank'])
+    #df['rnum'] = pd.to_numeric(df['rnum'])
+    #df['rank'] = pd.to_numeric(df['rank'])
     num_cols = ['rnum', 'rank', 'rankInten', 'salesAmt', 'audiCnt',  
                 'audiAcc', 'scrnCnt', 'showCnt', 'salesShare', 'salesInten', 
                 'salesChange', 'audiInten', 'audiChange']
-    for c in num_cols:
-        df[c] = pd.to_numeric(df[c])
+    #for c in num_cols:
+    #    df[c] = pd.to_numeric(df[c])
         
-    #df[num_cols] = df[num_cols].apply(pd.to_numeric)    
+    df[num_cols] = df[num_cols].apply(pd.to_numeric)    
     return df
 
 def save_data(ds_nodash):
@@ -37,21 +37,21 @@ def save_data(ds_nodash):
     print(sum_df)
 
 def save2df(load_dt="20120101", url_param = { }):
-    df = list2df(load_dt, url_param)
+    df = list2df(load_dt=load_dt, url_param=url_param)
     #df에 load_dt 칼럼 추가( 조회 일자 YYYYMMDD 형식으로)
     #df['load_dt'] = pd.Timestamp(df['load_dt'], str
     df['load_dt'] = load_dt
     #df = pd.concat([df,date], axis=0)
     #아래 파일 저장 시 load_dt 기본으로 파티셔닝
-    df.to_parquet('~/tmp/test_parquet', partition_cols=['load_dt'])
+    #df.to_parquet('~/tmp/test_parquet', partition_cols=['load_dt'])
     return df
 
-def list2df(load_dt="20120101", url_param = { }):
+def list2df(load_dt="20120101", url_param ={}):
     l = req2list(load_dt, url_param)
     df = pd.DataFrame(l)
     return df
 
-def req2list(load_dt="20120101",  url_param = { }) -> list:
+def req2list(load_dt="20120101",  url_param ={}) -> list:
     _, data = req(load_dt, url_param)
     l = data['boxOfficeResult']['dailyBoxOfficeList']
     return l
@@ -60,7 +60,7 @@ def get_key():
     key = os.getenv('MOVIE_API_KEY')
     return key
 
-def req(load_dt="20120101", url_param = { }):
+def req(load_dt="20120101", url_param = {}):
     #url = gen_url('20240720')
     url = gen_url(load_dt, url_param)
     r = requests.get(url)
@@ -70,7 +70,7 @@ def req(load_dt="20120101", url_param = { }):
     return code, data
 
 
-def gen_url(dt="20120101", url_param = { }):
+def gen_url(dt="20120101", url_param = {}):
     base_url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"
     key = get_key()
     url = f"{base_url}?key={key}&targetDt={dt}"
